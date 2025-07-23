7192e9bda3a2732099d641d7cfbb21cca50257aa
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-07-23 11:20:24 +0530

    docs: add better annotations to module

--- lua/template/module.lua
+++ lua/template/module.lua
@@ -1,16 +1,26 @@
 ---@class TemplateModule
 local M = {}
+
 local config = require('template.config').config
 
----@return string
+---Greet with a personalized message
+---@param name string? Name to greet, uses config.name if not provided
+---@return string greeting The greeting message
 M.greet = function(name)
     name = name and #name > 0 and name or config.name
 
-    M.show_notification('Hello ' .. name)
-    return 'Hello ' .. name
+    local greeting = 'Hello ' .. name
+    M.show_notification(greeting)
+    return greeting
 end
 
+---Show a notification with template branding
+---@param message string Message to display
 M.show_notification = function(message)
+    if not message or #message == 0 then
+        message = 'Hello from Template!'
+    end
+
     vim.notify(message, vim.log.levels.INFO, {
         title = 'Template',
         timeout = 5000,
