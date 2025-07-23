af0974b7931289d268ffd4c512a9ecf78466fa71
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-07-23 11:19:57 +0530

    feat: update template class

--- lua/template.lua
+++ lua/template.lua
@@ -1,6 +1,11 @@
-return {
-    setup = function(opts)
-        require('template.config').setup(opts)
-        require('template.commands').setup()
-    end,
-}
+---@class Template
+local M = {}
+
+---Setup template.nvim with user configuration
+---@param opts TemplateConfigOptions? User configuration options
+M.setup = function(opts)
+    require('template.config').setup(opts)
+    require('template.commands').setup()
+end
+
+return M
